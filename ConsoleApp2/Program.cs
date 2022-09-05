4dfec25e6cafe3677df9a4261d1f7ba7d4c41ff5
server <server@DESKTOP-GJQTT04>
2022-09-04 21:34:43 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Introduce tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre);
+
+var fecha= DateTime.Now.ToString("h:mm:ss");
+Console.WriteLine(fecha);
+
+
